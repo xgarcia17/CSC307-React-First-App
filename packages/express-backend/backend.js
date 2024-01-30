ed4eded4d55d0dac2677e8f881eb9a551c31829f
Xavier Garcia <xmangarcia17@gmail.com>
2024-01-30 00:56:32 -0800

    feat: add findUserByName and nodemon

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -43,7 +43,20 @@ const users = {
   ],
 };
 
-app.get("/users", (req, res) => {
-  res.send(users);
-});
+// app.get("/users", (req, res) => {
+//   res.send(users);
+// });
+const findUserByName = (name) => {
+  return users["users_list"].filter((user) => user["name"] === name);
+};
 
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
+});
