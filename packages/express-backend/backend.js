99312cfca86c9b4a46bee1381f1665d0d2900da0
Xavier Garcia <xmangarcia17@gmail.com>
2024-01-30 00:59:59 -0800

    feat: add findUserById

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -60,3 +60,16 @@ app.get("/users", (req, res) => {
     res.send(users);
   }
 });
+
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
